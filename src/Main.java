5d98cd62738a6fb7f7d8d8f59e8bf01aef3ffc52
yoonri <minyoonri0309@gmail.com>
2023-11-07 19:02:10 +0300

    try 2

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,14 @@
 public class Main {
     public static void main(String[] args) {
+
     }
-    public static String ageAndTemperature(int age, int temperature) {
-         ageAndTemperature(21, -7);
+
+    public static String ageAndTemperature(String name, int age, int temperature) {
+        ageAndTemperature("John", 54, 23);
+        String Stay = "stay at home";
+        System.out.println("Выживший" + Stay+ ".");
+
+        if (age > 15 && age < 45 && temperature > -10 && temperature < 30) ;
         {
             if (age > 15 && age < 45 && temperature < 5) {
                 System.out.println("go walking");
@@ -16,12 +22,9 @@ public class Main {
             if ((age > 45) && (temperature >= -10) && (temperature <= 16)) {
                 System.out.println("stay at home");
 
-            }return (ageAndTemperature(21,-7));
-
+            }return Stay;
         }
 
-
     }
+
 }
-
-
